eb6fe0378d7ab1277290b3047d1cd8e056cf260b
Devapriya P <devpriyaal@gmail.com>
2024-07-22 15:23:26 +0530

    Add files via upload

---
+++ bcdseg.c
@@ -0,0 +1,143 @@
+#include<LPC214X.h>
+void delay(unsigned int count);
+int main(){
+
+	
+IODIR0|=(0x01<<3);
+IODIR0|=(0x01<<4);
+IODIR0|=(0x01<<5);
+IODIR0|=(0x01<<6);
+IODIR0|=(0x01<<7);
+IODIR0|=(0x01<<8);
+IODIR0|=(0x01<<9);
+
+while(1){
+	
+//1
+	IOSET0|=(0x01<<4);
+	IOSET0|=(0x01<<5);
+	delay(200000);
+	IOCLR0 |=(0x01<<4);
+	IOCLR0 |=(0x01<<5);
+	
+	//2
+	IOSET0|=(0x01<<3);
+	IOSET0|=(0x01<<4);
+	IOSET0|=(0x01<<9);
+	IOSET0|=(0x01<<7);
+	IOSET0|=(0x01<<6);
+  delay(200000);
+	
+  IOCLR0 |=(0x01<<3);
+	IOCLR0 |=(0x01<<4);
+	IOCLR0 |=(0x01<<9);
+	IOCLR0 |=(0x01<<7);
+	IOCLR0 |=(0x01<<6);
+	//3
+	IOSET0|=(0x01<<3);
+	IOSET0|=(0x01<<4);
+	IOSET0|=(0x01<<9);
+	IOSET0|=(0x01<<5);
+	IOSET0|=(0x01<<6);
+  delay(200000);
+	
+  IOCLR0 |=(0x01<<3);
+	IOCLR0 |=(0x01<<4);
+	IOCLR0 |=(0x01<<9);
+	IOCLR0 |=(0x01<<5);
+	IOCLR0 |=(0x01<<6);
+//4
+  IOSET0|=(0x01<<8);
+	IOSET0|=(0x01<<4);
+	IOSET0|=(0x01<<9);
+	IOSET0|=(0x01<<5);
+	
+  delay(200000);
+	
+  IOCLR0 |=(0x01<<8);
+	IOCLR0 |=(0x01<<4);
+	IOCLR0 |=(0x01<<9);
+	IOCLR0 |=(0x01<<5);
+	//5
+	IOSET0|=(0x01<<3);
+	IOSET0|=(0x01<<8);
+	IOSET0|=(0x01<<9);
+	IOSET0|=(0x01<<5);
+	IOSET0|=(0x01<<6);
+  delay(200000);
+	
+  IOCLR0 |=(0x01<<3);
+	IOCLR0 |=(0x01<<8);
+	IOCLR0 |=(0x01<<9);
+	IOCLR0 |=(0x01<<5);
+	IOCLR0 |=(0x01<<6);
+//6
+	IOSET0|=(0x01<<8);
+	IOSET0|=(0x01<<5);
+	IOSET0|=(0x01<<9);
+	IOSET0|=(0x01<<7);
+	IOSET0|=(0x01<<6);
+	IOSET0|=(0x01<<3);
+  delay(200000);
+	
+  IOCLR0 |=(0x01<<3);
+	IOCLR0 |=(0x01<<8);
+	IOCLR0 |=(0x01<<5);
+	IOCLR0 |=(0x01<<9);
+	IOCLR0 |=(0x01<<7);
+	IOCLR0 |=(0x01<<6);
+	//7
+	IOSET0|=(0x01<<3);
+	IOSET0|=(0x01<<4);
+	
+	IOSET0|=(0x01<<5);
+
+  delay(200000);
+	
+  IOCLR0 |=(0x01<<3);
+	IOCLR0 |=(0x01<<4);
+
+	IOCLR0 |=(0x01<<5);
+	//8
+	IOSET0|=(0x01<<3);
+	IOSET0|=(0x01<<4);
+	IOSET0|=(0x01<<9);
+	IOSET0|=(0x01<<5);
+	IOSET0|=(0x01<<6);
+	IOSET0|=(0x01<<7);
+	IOSET0|=(0x01<<8);
+  delay(200000);
+	
+  IOCLR0 |=(0x01<<3);
+	IOCLR0 |=(0x01<<4);
+	IOCLR0 |=(0x01<<9);
+	IOCLR0 |=(0x01<<5);
+	IOCLR0 |=(0x01<<6);
+	IOCLR0 |=(0x01<<7);
+	IOCLR0 |=(0x01<<8);
+//9
+		IOSET0|=(0x01<<3);
+	IOSET0|=(0x01<<4);
+	IOSET0|=(0x01<<9);
+	IOSET0|=(0x01<<5);
+	IOSET0|=(0x01<<6);
+	IOSET0|=(0x01<<8);
+  delay(200000);
+	
+  IOCLR0 |=(0x01<<3);
+	IOCLR0 |=(0x01<<4);
+	IOCLR0 |=(0x01<<9);
+	IOCLR0 |=(0x01<<5);
+	IOCLR0 |=(0x01<<6);
+	IOCLR0 |=(0x01<<8);
+
+
+	}
+	
+}
+	
+void delay(unsigned int count)
+{
+	unsigned int i;
+	for(i=0;i<count;i++);
+}
